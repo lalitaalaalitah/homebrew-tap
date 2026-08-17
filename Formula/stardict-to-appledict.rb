d0a79b63af9ac685c3b833e8a55b00e376f305bb
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-18 00:22:28 +0530

    Update stardict-to-appledict formula to v1.3.0 and add patch-pyglossary

--- Formula/stardict-to-appledict.rb
+++ Formula/stardict-to-appledict.rb
@@ -2,8 +2,8 @@ class StardictToAppledict < Formula
   desc "StarDict to AppleDict conversion and installation utilities"
   homepage "https://github.com/lalitaalaalitah/stardict_to_appledict"
   url "https://github.com/lalitaalaalitah/stardict_to_appledict.git",
-      tag: "v1.2.0"
-  version "1.2.0"
+      tag: "v1.3.0"
+  version "1.3.0"
 
   depends_on "uv"
 
@@ -11,11 +11,14 @@ class StardictToAppledict < Formula
     bin.install "batch_dict_to_apple_dict.py" => "batch-dict-to-apple-dict"
     bin.install "install_sanskrit_dicts.py" => "install-sanskrit-dicts"
     bin.install "smart_batch_convert_and_release.py" => "smart-batch-convert-and-release"
+    bin.install "patch_pyglossary.py" => "patch-pyglossary"
+    bin.install "pyglossary-3.2.1"
   end
 
   test do
     system "#{bin}/batch-dict-to-apple-dict", "--version"
     system "#{bin}/install-sanskrit-dicts", "--version"
     system "#{bin}/smart-batch-convert-and-release", "--version"
+    system "#{bin}/patch-pyglossary", "--version"
   end
 end
