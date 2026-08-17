c549359f2cce9e856275035e5ac14667e566841e
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2026-08-18 05:28:32 +0530

    Update stardict-to-appledict formula to v1.5.2

--- Formula/stardict-to-appledict.rb
+++ Formula/stardict-to-appledict.rb
@@ -2,8 +2,8 @@ class StardictToAppledict < Formula
   desc "StarDict to AppleDict conversion and installation utilities"
   homepage "https://github.com/lalitaalaalitah/stardict_to_appledict"
   url "https://github.com/lalitaalaalitah/stardict_to_appledict.git",
-      tag: "v1.5.1"
-  version "1.5.1"
+      tag: "v1.5.2"
+  version "1.5.2"
 
   depends_on "uv"
 
